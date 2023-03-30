03740a66126492cb9443d67fb70336021da56df2
Guilherme A <guilhermealc01@gmail.com>
2023-03-30 14:26:13 -0300

    adicionando funcionalidade que extrai links de arquivo.md

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ function trataErro(erro) {
     // criando um novo objeto do tipo erro
 }
 
+function extraiLinks (texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const formataCaptura = capturas.map((resultado) => ({
+        [resultado[1]]: resultado[2]
+    }))
+    return formataCaptura
+}
+
 async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro)
     } finally {
