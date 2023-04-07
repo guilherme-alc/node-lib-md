335255b68c9ba7f52fbf91da58c00022cb57c8dd
Guilherme A <guilhermealc01@gmail.com>
2023-04-06 23:02:42 -0300

    branch main

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,8 +1,5 @@
-// a lista de links é uma lista de objetos, essa fun pecorre a lista e extrai os valores de cada objeto para uma nova array
 function separaLinks(arrLinks) { //recebe o resultado de links e nomes
-    return arrLinks.map((objetoLink) => Object.values(objetoLink).join()) // extraimos apenas os links e guardamos na fun
-    //método de objeto que extrai apenas valores de um objeto e joga dentro de um array
-    //o join é um método de array que pega o elemento de uma array e converte em string
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
 function trataErro(erro) {
@@ -13,12 +10,12 @@ function trataErro(erro) {
     }
 }
 
-async function checaStatus (arrUrl) { //recebe os links extraídos como parâmetro
-    const arrStatus = Promise.all( //o objeto Promise com o método .all vai receber a lista de promessas, resolver todas elas e retornar os resultados
-        arrUrl.map(async(url) =>{ //pra cada link chamamos a api fech
+async function checaStatus (arrUrl) {
+    const arrStatus = Promise.all(
+        arrUrl.map(async(url) =>{
             try {
-                const response = await fetch(url, {method: 'HEAD'}) //a api fetch vai retornar um json
-                return response.status//onde capturamos apenas a propriedade status para checar se o link está funcionando 200 = ok, 400 = erro
+                const response = await fetch(url, {method: 'HEAD'})
+                return response.status
             } catch (erro) {
                 return trataErro(erro);
             }
@@ -30,7 +27,7 @@ async function checaStatus (arrUrl) { //recebe os links extraídos como parâmet
 async function listaValidada(listaDeLinks) {
     const links = separaLinks(listaDeLinks)
     const status = await checaStatus(links)
-    return listaDeLinks.map((objLink, i) => ({ //montando o objeto da array de links com o status inserido
+    return listaDeLinks.map((objLink, i) => ({
         ...objLink, 
         status: status[i]
     }))
